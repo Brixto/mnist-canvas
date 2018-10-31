9744215163d4f5352a67e4e8674e0da833047c2b
Jonas Fröber <jonasfroeber@Jonass-iMac.local>
2018-10-31 17:00:08 +0100

    draw on canvas

--- index.js
+++ index.js
@@ -4,9 +4,31 @@ var context = canvas.getContext("2d");
 var w = canvas.width;
 var h = canvas.height;
 
+context.canvas.width = 960;
+context.canvas.height = 960;
+
+var pos = { x: 0, y: 0 };
+
+function setPosition(e) {
+  pos.x = e.clientX - canvas.offsetLeft;
+  pos.y = e.clientY - canvas.offsetTop;
+}
+
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', setPosition);
+canvas.addEventListener('mouseenter', setPosition);
 
 function draw(e) {
-    console.log(e.clientX - canvas.offsetLeft);
-    console.log(e.clientY - canvas.offsetTop);
+    if(e.buttons !== 1) return;
+    context.beginPath();
+
+    context.lineWidth = 10;
+    context.lineCap = 'round';
+    context.strokeStyle = 'red';
+
+    context.moveTo(pos.x, pos.y);
+    setPosition(e);
+    context.lineTo(pos.x, pos.y);
+
+    context.stroke();
 }
